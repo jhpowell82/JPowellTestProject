3d3fdac4ebb72f9de50e575e961b11a2723b3795
unknown <jpowell@jpowelllt.askcts.com>
2012-04-12 10:57:58 -0500

    Changes to js file to reflect my repo.

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,6 +1,6 @@
 window.addEvent('domready', function() {
     new Request.HTML({
-        url: '/gh/get/response.html/zalun/jsFiddleGithubDemo/tree/master/Demo/',
+        url: '/gh/get/response.html/jhpowell82/JPowellTestProject/tree/master/Demo',
         data: {'delay': 1},
         method: 'post',
         update: 'demo',
