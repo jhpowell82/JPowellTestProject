b95a175e1b090f6a109cf2e8f985f2b128a4345a
unknown <jpowell@jpowelllt.askcts.com>
2012-04-12 11:29:10 -0500

    Updating to my javascript tricks files.

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,12 +1,4 @@
 window.addEvent('domready', function() {
-    new Request.HTML({
-        url: '/gh/get/response.html/jhpowell82/JPowellTestProject/tree/master/Demo/',
-        data: {'delay': 1},
-        method: 'post',
-        update: 'demo',
-        onSuccess: function(response) {
-            $('demo').highlight();
-        }
-    }).send();
+    $("#contactForm").validate();
 })
 ?
